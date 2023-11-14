f82f159e78dd015ad37187b6503611abf95953c0
mellwin <oaparamonov@mail.ru>
2023-11-14 15:43:25 +0300

    add Module5

---
+++ 5 Методы/Task 1 Метод разделения строки на слова/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static string[] getParseText(string _text)
+        {
+            String[] _masText = _text.Split(new char[] { ' ' });
+
+            return _masText;
+        }
+
+        static void printText(string[] _masText)
+        {
+            for (int i = 0; i < _masText.GetLength(0); i++)
+            {
+                Console.WriteLine(_masText[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+
+            string text = Console.ReadLine();
+
+            string[] masText = getParseText(text);
+
+            printText(masText);
+        }
+    }
+}
---
+++ 5 Методы/Task 2 Перестановка слов в предложении/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static string[] getParseText(string _text)
+        {
+            String[] _masText = _text.Split(new char[] { ' ' });
+
+            return _masText;
+        }
+
+        public static string ReversWords(string _text)
+        {
+            string ResultString = "";
+            String[] masText = getParseText(_text);
+
+            for (int i = masText.GetLength(0) - 1; i >= 0; i--)
+            {
+                ResultString += masText[i] + " ";
+            }
+
+            return ResultString;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+
+            string text = Console.ReadLine();
+
+            Console.WriteLine(ReversWords(text));
+            Console.ReadKey();
+        }
+    }
+}
